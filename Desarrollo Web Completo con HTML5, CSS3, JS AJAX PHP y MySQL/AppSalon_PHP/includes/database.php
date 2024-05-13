456e296bcf0f7caa366098127c1e6e4397bf2303
Johnny <jjimenezdiaz96@gmail.com>
2024-05-13 14:26:06 +0200

    Elimino directorios que aun o se han usado

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/AppSalon_PHP/includes/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = mysqli_connect("localhost","root","Ilerna861996","appsalon");
-
-if (!$db){
-    echo "Conexion fallida";
-    exit;
-} 
-
---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/06.js
@@ -0,0 +1,15 @@
+// OBJETO MATH
+
+let resultado;
+
+resultado = Math.PI; // numero pi
+resultado = Math.round(2.7); // redondea
+resultado = Math.ceil(2.9); // redondea hacia arriba siempre
+resultado = Math.floor(2.9) // redondea hacia abajo siempre 
+resultado = Math.sqrt(144); // Raiz cuadrada
+resultado = Math.abs(-144); // convierte en positivo
+resultado = Math.min(3, 5, 1 ,8 , 10); // saca el minimo de un listado de nuneros
+resultado = Math.max(3, 5, 1 ,8 , 10); // saca el maximo de un listado de nuneros
+resultado = Math.floor(Math.random() * 30); // saca el maximo de un listado de nuneros
+
+console.log(resultado);
