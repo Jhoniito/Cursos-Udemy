9c1e752c3f60652f8fa009a2269cb18a6df48558
Johnny <jjimenezdiaz96@gmail.com>
2024-05-23 17:40:27 +0200

    .

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/14.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/14.js
@@ -23,3 +23,7 @@ console.log(meses.length);
 numeros.forEach(function(numero){
     console.log(numero)
 })
+
+numeros.push(60,70,80); // Se agrega al final del arreglo
+
+numeros.unshift(-10,-20,-30); // Se agrega al principio del arreglo
